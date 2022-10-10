e04c21c82cede053998b8718917f81fc51b5a64f
OliveiraDCI <oliveira.sein.dci@gmail.com>
2022-10-10 15:22:34 +0200

    Exercise concluded.

--- src/index.js
+++ src/index.js
@@ -2,7 +2,24 @@ import React from "react"; //Main React.js library
 import ReactDOM from "react-dom/client"; //we use ReactDOM to render into the DOM
 
 // only update the value of this array
-const navlinkItems = [];
+const numbers = [1, 2, 3];
+const links = [
+  "https://www.google.com",
+  "https://www.facebook.com",
+  "https://www.amazon.com",
+];
+const text = [
+  "Link to google.com",
+  "Link to facebook.com",
+  "Link to amazon.com",
+];
+const navlinkItems = numbers.map((number) => (
+  <li key={number.toString()} className="nav-item">
+    <a className="nav-link" href={links[number - 1]}>
+      {text[number - 1]}
+    </a>
+  </li>
+));
 
 const content = <ul className="nav">{navlinkItems}</ul>;
 
